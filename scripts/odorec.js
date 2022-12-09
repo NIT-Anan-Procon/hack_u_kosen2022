54e48a4edf8cda31912006a3f97c2ccac042a25f
ShoeiAzekawa <1206079@st.anan-nct.ac.jp>
2022-12-09 19:56:00 +0900

    ボリュームの大きさで表示されるgifを変更する

--- scripts/odorec.js
+++ scripts/odorec.js
@@ -36,15 +36,15 @@ function setup() {
   });
 
   // オーディオストリームの生成
-  let audioContext = new (window.AudioContext || window.webkitAudioContext)()
+  let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
   // 音声入力の開始
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
           // メディアストリームソースとメーターの生成
-          let mediaStreamSource = audioContext.createMediaStreamSource(stream)
-          let meter = createAudioMeter(audioContext)
-          mediaStreamSource.connect(meter)
+          let mediaStreamSource = audioContext.createMediaStreamSource(stream);
+          let meter = createAudioMeter(audioContext);
+          mediaStreamSource.connect(meter);
       })
   }
 }
@@ -79,7 +79,8 @@ function Test() {
       .then(function (data) {
         console.log(data["variable"]);
       });
-  }, 700);
+      //2秒間隔でデータ送信
+  }, 2000);
 }
 
 //CSSを埋め込んでる
@@ -95,12 +96,12 @@ function addVideo() {
   //reset();
 
   //1. innerHTMLの兄弟
-  var $videoDiv = $.parseHTML('<div class="shia-do-it"><div class="container"><img height=60 name="media"></div></div>');
+  var $videoDiv = $.parseHTML('<div class="shia-do-it" id="target"><div class="container"><img height=60 name="media"></div></div>');
   $('body').append($videoDiv);
 
   //2. 1で作ったdivにvideoをぶち込む
   var video = $($videoDiv).find('img').get(0);
-  var filename = 'assets/hiyoko.gif';
+  var filename = 'assets/lovegun.gif';
   video.src = chrome.extension.getURL(filename);
 
   video.onended = function () {
@@ -145,65 +146,98 @@ function removeVideo(showRateMe) {
 // メーターの生成
 function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
   // メーターの生成
-  const processor = audioContext.createScriptProcessor(512)
-  processor.onaudioprocess = volumeAudioProcess
-  processor.clipping = false
-  processor.lastClip = 0
-  processor.volume = 0
-  processor.clipLevel = clipLevel || 0.98
-  processor.averaging = averaging || 0.95
-  processor.clipLag = clipLag || 750
-  processor.connect(audioContext.destination)
+  const processor = audioContext.createScriptProcessor(512);
+  processor.onaudioprocess = volumeAudioProcess;
+  processor.clipping = false;
+  processor.lastClip = 0;
+  processor.volume = 0;
+  processor.clipLevel = clipLevel || 0.98;
+  processor.averaging = averaging || 0.95;
+  processor.clipLag = clipLag || 750;
+  processor.connect(audioContext.destination);
 
   // クリップチェック時に呼ばれる
   processor.checkClipping = function () {
       if (!this.clipping) {
-          return false
+          return false;
       }
       if ((this.lastClip + this.clipLag) < window.performance.now()) {
-          this.clipping = false
+          this.clipping = false;
       }
-      return this.clipping
+      return this.clipping;
   }
 
   // シャットダウン時に呼ばれる
   processor.shutdown = function () {
-      this.disconnect()
-      this.onaudioprocess = null
+      this.disconnect();
+      this.onaudioprocess = null;
   }
 
-  return processor
+  return processor;
 }
 
 // オーディオ処理時に呼ばれる
 function volumeAudioProcess(event) {
-  const buf = event.inputBuffer.getChannelData(0)
-  const bufLength = buf.length
-  let sum = 0
-  let x
+  const buf = event.inputBuffer.getChannelData(0);
+  const bufLength = buf.length;
+  let sum = 0;
+  let x;
 
   // 平均ボリュームの計算
   for (var i = 0; i < bufLength; i++) {
-      x = buf[i]
+      x = buf[i];
       if (Math.abs(x) >= this.clipLevel) {
-          this.clipping = true
-          this.lastClip = window.performance.now()
+          this.clipping = true;
+          this.lastClip = window.performance.now();
       }
-      sum += x * x
+      sum += x * x;
   }
-  const rms = Math.sqrt(sum / bufLength)
+  const rms = Math.sqrt(sum / bufLength);
   //計算には二乗平均平方根(RMS: root mean square）を用いる
-  this.volume = Math.max(rms, this.volume * this.averaging)
+  this.volume = Math.max(rms, this.volume * this.averaging);
 
   // ボリュームの表示
   if (this.volume - previousVolume > 0.05) {
-      console.log(this.volume - previousVolume)
+      var judge = this.volume - previousVolume;
+      console.log(judge);
+      //ボリュームの大きさで画像を変える
+      changeVideo(judge);
   }
-  previousVolume = this.volume
+  previousVolume = this.volume;
 }
 
+//ボリュームの大きさで画像を変える
+function changeVideo(judge) {
+  //今出てる画像を削除する。
+  document.getElementById('target').remove();
 
-
+  //新しい画像をページに追加する
+  var $videoDiv = $.parseHTML('<div class="shia-do-it" id="target"><div class="container"><img height=60 name="media"></div></div>');
+  $('body').append($videoDiv);
+  var video = $($videoDiv).find('img').get(0);
+  var filename = 'assets/headphone.gif';
+  
+  if(judge > 0.12) {
+    filename = 'assets/flower.gif';
+    console.log('your voice is big!' + filename);
+    
+  }else if(judge > 0.09) {
+    filename = 'assets/headphone.gif';
+    console.log('your voice is so-so' + filename);
+    
+  }else if(judge > 0.07) {
+    filename = 'assets/bonus.gif';
+    console.log('your voice is small..' + filename);
+    
+  }else if(judge > 0.05) {
+    filename = 'assets/lovegun.gif';
+    console.log('your voice is so small...' + filename);
+    
+  }else {
+    console.log('bag');
+  }
+  video.src = chrome.extension.getURL(filename);
+}
 
 /*
 var BANNER_APPEAR_DELAY = 1000 * 0.5;
